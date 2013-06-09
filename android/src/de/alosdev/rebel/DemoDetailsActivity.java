d390007e70a4178ac32a880f7d100a798a386be6
alosdev <hasanhosgel@googlemail.com>
2013-06-09 12:25:31 +0200

    show report markers

--- android/src/de/alosdev/rebel/DemoDetailsActivity.java
+++ android/src/de/alosdev/rebel/DemoDetailsActivity.java
@@ -23,18 +23,27 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
+import android.util.JsonReader;
 import android.util.JsonWriter;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -48,6 +57,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -55,6 +65,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import de.alosdev.rebel.dialog.ReportDialog;
 import de.alosdev.rebel.dialog.ReportDialog.ReportDialogListener;
 import de.alosdev.rebel.domain.DemoDetails;
+import de.alosdev.rebel.domain.Report;
+import de.alosdev.rebel.view.DemoView;
 
 /**
  * This demo shows how GMS Location can be used to check for changes to the
@@ -63,7 +75,7 @@ import de.alosdev.rebel.domain.DemoDetails;
  * on the map, we request updates from the {@link LocationClient}.
  */
 public class DemoDetailsActivity extends FragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener,
-    LocationListener, ReportDialogListener {
+    LocationListener, ReportDialogListener, OnItemClickListener {
 
 	private static final String TAG = DemoDetailsActivity.class.getSimpleName();
 	private static final String BUNDLE_DETAIL = TAG + ".demoDetails";
@@ -74,6 +86,8 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 	                                                                                          // seconds
 	    .setFastestInterval(16) // 16ms = 60fps
 	    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	private static final String BUNDLE_REPORTS = TAG + ".reports";
+	private static final Report LOADING_DETAILS = new Report(null, "Loading", null);
 
 	public static void start(Activity activity, DemoDetails details) {
 		Intent startIntent = new Intent(activity, DemoDetailsActivity.class);
@@ -84,8 +98,9 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 	private GoogleMap mMap;
 	private LocationClient mLocationClient;
 	private DemoDetails details;
-	private ListView list;
 	private Location lastLocation;
+	boolean loading = false;
+	private CustomArrayAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -94,7 +109,34 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 		details = getIntent().getParcelableExtra(BUNDLE_DETAIL);
 		getActionBar().setTitle(details.title);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		list = (ListView) findViewById(R.id.list);
+		ListView list = (ListView) findViewById(R.id.list);
+
+		final ArrayList<Report> items;
+		if (null == savedInstanceState) {
+			items = new ArrayList<Report>();
+		} else
+			items = savedInstanceState.getParcelableArrayList(BUNDLE_REPORTS);
+		adapter = new CustomArrayAdapter(getApplicationContext(), items);
+		list.setAdapter(adapter);
+		list.setOnItemClickListener(this);
+		if (null == savedInstanceState)
+			refresh();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		final ArrayList<Report> list = getReports();
+
+		outState.putParcelableArrayList(BUNDLE_REPORTS, list);
+		super.onSaveInstanceState(outState);
+	}
+
+	private ArrayList<Report> getReports() {
+		final int count = adapter.getCount();
+		final ArrayList<Report> list = new ArrayList<Report>(count);
+		for (int i = 0; i < count; i++)
+			list.add(adapter.getItem(i));
+		return list;
 	}
 
 	@Override
@@ -102,6 +144,30 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 		if (item.getItemId() == android.R.id.home)
 			onBackPressed();
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_start, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		MenuItem item = menu.findItem(R.id.refresh);
+		if (null != item)
+			item.setEnabled(!loading);
+
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.refresh) {
+			refresh();
+			return true;
+		}
+		return super.onContextItemSelected(item);
 	}
 
 	@Override
@@ -113,10 +179,19 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 		UiSettings uiSettings = mMap.getUiSettings();
 		uiSettings.setZoomControlsEnabled(false);
 		uiSettings.setCompassEnabled(true);
-		mMap.clear();
+		drawMarkers();
+	}
+
+	private void drawMarkers() {
+	  mMap.clear();
 		for (Location loc : details.route)
 			mMap.addMarker(new MarkerOptions().position(new LatLng(loc.getLatitude(), loc.getLongitude())));
-	}
+		for (Report rep : getReports()) {
+			if (null != rep.location)
+				mMap.addMarker(new MarkerOptions().position(new LatLng(rep.location.getLatitude(), 
+						rep.location.getLongitude())).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+		}
+  }
 
 	@Override
 	public void onPause() {
@@ -175,9 +250,16 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 		LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
 		if (null != lastLocation)
 			boundsBuilder.include(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()));
-		boundsBuilder.include(new LatLng(location.getLatitude(), location.getLongitude()));
+		if (location != null)
+			boundsBuilder.include(new LatLng(location.getLatitude(), location.getLongitude()));
 		for (Location loc : details.route)
 			boundsBuilder.include(new LatLng(loc.getLatitude(), loc.getLongitude()));
+		for (Report report : getReports()) {
+			if (report.location != null) {
+				Log.e(TAG, "get reports");
+				boundsBuilder.include(new LatLng(report.location.getLatitude(), report.location.getLongitude()));
+			}
+		}
 
 		mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 130));
 	}
@@ -230,14 +312,14 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 						writeBody(out, name, message);
 						out.close();
 
-						return  "reported name: " + name + "; message:" + message;
+						return "reported name: " + name + "; message:" + message;
 					} catch (MalformedURLException e) {
 						return "cannot send report";
 					} catch (IOException e) {
 						return "cannot send report";
 					}
 				}
-				
+
 				@Override
 				protected void onPostExecute(String result) {
 					Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
@@ -262,5 +344,108 @@ public class DemoDetailsActivity extends FragmentActivity implements ConnectionC
 
 		writer.endObject();
 		writer.close();
+	}
+
+	private static class CustomArrayAdapter extends ArrayAdapter<Report> {
+		public CustomArrayAdapter(Context context, ArrayList<Report> items) {
+			super(context, R.layout.feature, R.id.title, items);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			DemoView demoView;
+			if (convertView instanceof DemoView) {
+				demoView = (DemoView) convertView;
+			} else {
+				demoView = new DemoView(getContext());
+			}
+
+			Report demo = getItem(position);
+
+			demoView.setTitleId(demo.report);
+			demoView.setDescriptionId("by " + demo.name);
+
+			return demoView;
+		}
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	void refresh() {
+		loading = true;
+		adapter.clear();
+		adapter.add(LOADING_DETAILS);
+		new RefreshAsyncTask().execute();
+		invalidateOptionsMenu();
+	}
+
+	class RefreshAsyncTask extends AsyncTask<Integer, Void, ArrayList<Report>> {
+
+		@Override
+		protected ArrayList<Report> doInBackground(Integer... params) {
+			JsonReader reader = null;
+			try {
+				// TODO
+				// HttpURLConnection conn = (HttpURLConnection) new
+				// URL("http://rebel.polizei-news.com/test.php")
+				// .openConnection();
+				// conn.setReadTimeout(10000 /* milliseconds */);
+				// conn.setConnectTimeout(15000 /* milliseconds */);
+				// conn.setRequestMethod("GET");
+				// conn.setDoInput(true);
+				// // Starts the query
+				// conn.connect();
+				// reader = new JsonReader(new InputStreamReader(conn.getInputStream(),
+				// "UTF-8"));
+				try {
+					Thread.sleep(4000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return readDemoDetailsArray(reader);
+				// } catch (MalformedURLException e) {
+				// Log.e(TAG, "cannot parse document", e);
+				// } catch (IOException e) {
+				// Log.e(TAG, "cannot parse document", e);
+			} finally {
+				if (null != reader)
+					try {
+						reader.close();
+					} catch (IOException e) {
+					}
+			}
+
+			// return new ArrayList<Report>();
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<Report> result) {
+			adapter.clear();
+			adapter.addAll(result);
+			loading = false;
+			setUpMapIfNeeded();
+			createBounds(null);
+			drawMarkers();
+			invalidateOptionsMenu();
+		}
+
+	}
+
+	private ArrayList<Report> readDemoDetailsArray(JsonReader reader) {
+		ArrayList<Report> result = new ArrayList<Report>();
+		Location location = new Location("m");
+		location.setLatitude(52d);
+		location.setLongitude(13d);
+		result.add(new Report(null, "my new report", location));
+		location = new Location("m");
+		location.setLatitude(52d);
+		location.setLongitude(12d);
+		result.add(new Report("alosdev", "my newer report", location));
+		return result;
 	}
 }
